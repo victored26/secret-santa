bdf9efa5cd58c91d6bab9c70b75cbfebe4d02e4e
Victor <vfernandez.fw@gmail.com>
2024-11-23 08:56:54 -0600

    Proof of concept

--- concept.py
+++ concept.py
@@ -2,14 +2,20 @@
 
 from random import randint
 
-def assignID(names: list[str]):
+def secret_santa(names: list[str]) -> dict:
+    if len(names) < 3:
+        return
+    pairings = pairIDs(len(names))
+    return pairNames(assignID(names), pairings)
+
+def assignID(names: list[str]) -> dict:
     """Creates a dictionary where each name is assigned an integer"""
     name_id = {}
     for j in range(len(names)):
         name_id[j+1] = names[j]
     return name_id
 
-def pairIDs(length: int):
+def pairIDs(length: int) -> dict:
     """Pairs IDs such that no ID is paired with itself and no two pairs
     of IDs are exclusively paired to each other"""
     # For these conditions to always hold, the IDs must be assigned such that
@@ -26,3 +32,9 @@ def pairIDs(length: int):
         drawn.add(giftee)
     pairings[giftee] = 1
     return pairings
+
+def pairNames(namesID: dict, pairings: dict) -> dict:
+    secret_santa = {}
+    for key, value in pairings.items():
+        secret_santa[namesID[key]] = namesID[value]
+    return secret_santa
