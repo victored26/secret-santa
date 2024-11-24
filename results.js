787304b20f222e9babf06bd5584e52d711940fbd
Victor <vfernandez.fw@gmail.com>
2024-11-24 17:57:42 -0600

    Part IV

--- results.js
+++ results.js
@@ -1,22 +1,45 @@
 const secretSanta = JSON.parse(sessionStorage.getItem('secretSanta'));
 const results = document.getElementById("results");
-console.log(secretSanta);
+const downloads = document.getElementById("downloads");
 displaySecretSanta();
 
 function displaySecretSanta() {
     for (let [gifter, giftee] of Object.entries(secretSanta)) {
-        let node = document.createElement("h4");
+        let node = document.createElement("h3");
         node.innerHTML = `${gifter} &#8702; ${giftee}`;
         results.appendChild(node);
     }
+    downloadJSON();
+    downloadCSV();
 }
 
-function secretSantaJSON(secretSanta) {
-    /* Converts the secret santa to a JSON */
-    return JSON.stringify(secretSanta);
+function downloadJSON() {
+    const blob = new Blob(
+        [JSON.stringify(secretSanta, null, 2)],
+        {type: 'application/json'}
+    );
+    const url = URL.createObjectURL(blob);
+    const node = document.createElement("a");
+    node.setAttribute("href", url);
+    node.setAttribute("download", "secret_santa.json");
+    node.textContent = "Download as JSON";
+    downloads.appendChild(node); 
 }
 
-function secretSantaCSV(secretSanta) {
+function downloadCSV() {
+    const blob = new Blob(
+        [secretSantaCSV()],
+        {type: 'application/csv'}
+    );
+    const url = URL.createObjectURL(blob);
+    const node = document.createElement("a");
+    node.setAttribute("href", url);
+    node.setAttribute("download", "secret_santa.csv");
+    node.textContent = "Download as CSV";
+    downloads.appendChild(node); 
+}
+
+function secretSantaCSV() {
     /* Converts the secret santa to a CSV */
     const csvRows = ["gifter,giftee"];
     for (let [gifter, giftee] of Object.entries(secretSanta)) {
